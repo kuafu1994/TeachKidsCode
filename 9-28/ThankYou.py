f18fd14d3897b2fdcd2ca8bfa32e208fe3587fab
Pengfei Zhang <pfzhang@cse.cuhk.edu.hk>
2019-09-27 22:32:50 +0800

    9-28

---
+++ 9-28/ThankYou.py
@@ -0,0 +1,10 @@
+
+my_name = "Pengfei"
+my_age = 24
+
+Your_name = input("what is your name? ")
+Your_age = input("what is your age? ")
+
+print("My name is", my_name, ", and I am", my_age, "year old." )
+print("Your name is", Your_name, ", and you are", Your_age, ".")
+print("Thank you for taling my course,", Your_name)
