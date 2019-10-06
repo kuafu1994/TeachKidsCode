9aa809de659558401c94b0ab1d50372b2caebae1
Pengfei Zhang <pfzhang@cse.cuhk.edu.hk>
2019-10-06 12:55:53 +0800

    10-7

---
+++ 10-7/simple-game.py
@@ -0,0 +1,10 @@
+
+temp = input("Guess a number that match what I am thinking now? ")
+guess = int(temp)
+
+if guess == 8:
+    print("Bingo, you are right!")
+
+else:
+    print("You are wrong, I am thinking 8.")
+    
